3f23aa929f5589ce55e4621713339b4ced778ed3
unknown <iamrifathasbi@gmaile.com>
2023-05-21 13:53:12 +0700

    update background

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+const canvas = document.querySelector('canvas')
+const board = canvas.getContext('2d')
+
+canvas.width = DISPLAY_PIXEL.width * ASPECT_RATIO.width
+canvas.height = DISPLAY_PIXEL.height * ASPECT_RATIO.height
+
+const playerProperty = {
+    width : 50,
+    height : 50,
+    speed : 5,
+    color :'tomato',
+    position : {
+        x : 0,
+        y : 0
+    }
+}
+
+const enemyProperty = {
+    width : 50,
+    height : 50,
+    speed : 2,
+    color :'black',
+    position : {
+        x : canvas.width - 50,
+        y : canvas.height - 50
+    }
+}
+
+const ground = new Ground('/src/assets/bg.jpg', canvas.width, canvas.
+height)
+const player = new Player(playerProperty)
+const enemy = new Player(enemyProperty)
+
+
+function animate() {
+    ground.create()
+    player.create()
+    enemy.create()
+    player.update()
+    window.requestAnimationFrame(animate)
+}
+
+    window.addEventListener('keydown', function(callback){
+        player.movement(callback.key)
+    })
+
+animate()
